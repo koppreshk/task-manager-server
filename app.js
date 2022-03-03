95dd3a41a6267f2cc544774a9ce62d44099bba27
koppresh <koppresh.putpak@agilepoint.com>
2022-03-03 17:31:56 +0530

    refactor: code cleanup

--- app.js
+++ app.js
@@ -10,13 +10,9 @@ const PORT = 9000;
 //Middleware
 app.use(express.json());
 
-app.get('/hello', (request, response) => {
-    response.send('Task Manager App');
-})
-
 app.use('/api/v1/tasks', tasks);
 
-const start = async() => {
+const init = async() => {
     try {
         await establishDBConnection(process.env.MONGO_URI)
         app.listen(PORT, () => console.log('Server started at http://localhost:9000'))
@@ -25,15 +21,4 @@ const start = async() => {
     }
 }
 
-//API's Used currently:
-/*
-          URL         | METHOD |   Name
-    _________________________________________      
-
-    /api/v1/tasks     | GET    |  GetAllTasks
-    /api/v1/tasks     | POST   |  CreateTask
-    /api/v1/tasks/:id | GET    |  GetTask
-    /api/v1/tasks/:id | PATCH  |  UpdateTask
-    /api/v1/tasks/:id | DELETE |  deleteTask
-*/
-start()
+init();
