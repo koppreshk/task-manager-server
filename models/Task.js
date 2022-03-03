1377e785b3d6c65b798a974a27658773bb97d1e5
koppresh <koppresh.putpak@agilepoint.com>
2022-03-03 15:52:34 +0530

    added validation methods for creating tasks

--- models/Task.js
+++ models/Task.js
@@ -1,8 +1,16 @@
 const mongoose = require('mongoose');
 
 const TaskSchema = new mongoose.Schema({
-    name: String,
-    completed: Boolean
+    name: {
+        type: String,
+        required: [true, 'Name is required'],
+        trim: true,
+        maxlength: [25, 'Maximum 25 characters allowed']
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    }
 });
 
 module.exports = mongoose.model('Task', TaskSchema)
