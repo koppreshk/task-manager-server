622ba23bf889a2d4db6fdf8d04f5345696931fc8
koppresh <koppresh.putpak@agilepoint.com>
2022-03-07 17:56:40 +0530

    added 'route not found'

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const establishDBConnection = require('./db/connect');
 const express = require('express');
 const tasks = require('./routes/tasks');
 require('dotenv').config();
+const notFound = require('./middleware/route-not-found');
 
 //Initialize express
 const app = express();
@@ -10,8 +11,9 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/v1/tasks', tasks);
+app.use(notFound);
 
-const init = async() => {
+const init = async () => {
     try {
         await establishDBConnection(process.env.MONGO_URI)
         app.listen(process.env.PORT, () => console.log(`Server started at http://localhost:${process.env.PORT}`))
